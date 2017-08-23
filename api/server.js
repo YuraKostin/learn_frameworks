b1d354e9be9651fe28cd644690d5215c5ab37d8d
JuriyKostin <juriy.kostin@ncloudtech.ru>
2017-08-23 15:19:44 +0300

    Init

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const express = require('express');
+const app = express();
+app.get('/users', (req, res) => {
+    fs.readFile('./test.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+
+        res.json({users: JSON.parse(data)});
+    });
+});
+
+app.listen('8000');
